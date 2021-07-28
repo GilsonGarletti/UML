113ecba1b4d505f0434733ac2402b7982da738ba
Gilson <garletti@hotmail.com>
2021-07-28 18:27:27 -0300

    V-II

---
+++ cursocm/src/test/java/com/utl/cursocm/CursocmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utl.cursocm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursocmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
